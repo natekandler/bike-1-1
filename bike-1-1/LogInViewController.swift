e684c3295135d23fc47bbeabbf17f207fb3bd395
Nate Kandler <natekandler@gmail.com>
2014-06-16 16:30:16 -0500

    begins work on login

---
+++ bike-1-1/LogInViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LogInViewController.swift
+//  bike-1-1
+//
+//  Created by Nate Kandler on 6/16/14.
+//  Copyright (c) 2014 Apprentice. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    
+    @IBOutlet var emailField : UITextField
+    
+    @IBOutlet var passwordField : UITextField
+    
+    @IBAction func loginButton(sender : AnyObject) {
+        if emailField.text == "natekandler@gmail.com" && passwordField.text == "password" {
+        
+        }
+        
+        else {
+        
+        }
+        
+        
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
